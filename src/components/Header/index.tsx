6c097b1d924cef9eb8df72412d9f7b4978e33e90
Flávio Renato <peubevbr@gmail.com>
2023-04-15 15:16:19 -0300

    Criação da chamada a API

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContent, HeaderContianer, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/Logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -8,7 +10,14 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            {/* Dialog.Tigger é um botão específico do Dialog que abre o modal. Como Já temos um botão para essa função, informamos que ele recebe um botão filho */}
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContianer>
   )
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,45 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      {/* Portal - Faz com que o conteúdo do modal fique fora da estrutura da aplicação */}
+      <Overlay /> {/* Overlay - Tela de fundo o Dialog */}
+      <Content>
+        {/* Content - conteúdo do Dialog */}
+        <Dialog.Title>Nova Transação</Dialog.Title>
+        <CloseButton>
+          {/* Close - botão de fechar o motao */}
+          <X />
+        </CloseButton>
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
+}

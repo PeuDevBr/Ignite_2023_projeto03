5b46ba7799ecb1a4b8b80d05da6ac2aa26c54cde
Flávio Renato <peubevbr@gmail.com>
2023-04-17 21:39:46 -0300

    Conexão com a API - Buscando transações

--- src/Contexts/TransactionsContext.tsx
+++ src/Contexts/TransactionsContext.tsx
@@ -12,6 +12,8 @@ interface TransactionProps {
 interface TransactionContextType {
   transactions: TransactionProps[]
   // retorna uma lista de transações do tipo TransactionProps
+  fetchTransactions: (search?: string) => Promise<void>
+  // Promisse: função assíncrona - <void>: sem retorno
 }
 
 interface TransactionsProviderProps {
@@ -22,18 +24,27 @@ export const TransactionContext = createContext({} as TransactionContextType)
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<TransactionProps[]>([])
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
+
+  async function fetchTransactions(search?: string) {
+    const url = new URL('http://localhost:3333/transactions')
+
+    if (search) {
+      url.searchParams.append('q', search)
+    }
+
+    const response = await fetch(url)
     const data = await response.json()
+
+    console.log(data)
 
     setTransactions(data)
   }
   useEffect(() => {
-    loadTransactions()
+    fetchTransactions()
   }, [])
 
   return (
-    <TransactionContext.Provider value={{ transactions }}>
+    <TransactionContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionContext.Provider>
   )
